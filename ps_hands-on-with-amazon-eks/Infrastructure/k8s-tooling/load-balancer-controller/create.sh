df61a46e26d20261d96023b85456a42dc1b08c06
Your Name <you@example.com>
2025-01-29 08:16:54 +0530

    changes

--- ps_hands-on-with-amazon-eks/Infrastructure/k8s-tooling/load-balancer-controller/create.sh
+++ ps_hands-on-with-amazon-eks/Infrastructure/k8s-tooling/load-balancer-controller/create.sh
@@ -1,3 +1,5 @@
+# AWS Loadbalancer Github Repo: https://github.com/kubernetes-sigs/aws-load-balancer-controller/tree/main
+
 helm repo add eks https://aws.github.io/eks-charts
 
 helm upgrade --install \

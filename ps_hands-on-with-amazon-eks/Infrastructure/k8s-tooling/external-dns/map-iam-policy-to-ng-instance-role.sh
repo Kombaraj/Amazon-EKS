2b0fea1b4272403ad21f5b3d25b3c5a80d925e8e
Your Name <you@example.com>
2025-02-01 10:05:19 +0530

    changes

--- ps_hands-on-with-amazon-eks/Infrastructure/k8s-tooling/external-dns/map-iam-policy-to-ng-instance-role.sh
+++
@@ -1,5 +0,0 @@
-aws iam attach-role-policy \
-    --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess \
-    --role-name <<ROLE>>
-
-echo "IAM Policy mapped to NodeGroup Instance Role ro get access to Route53..."

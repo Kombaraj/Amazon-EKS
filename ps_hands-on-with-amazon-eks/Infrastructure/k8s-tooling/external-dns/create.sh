9296a714034dd7628e193777b1c5338b6509477a
Your Name <you@example.com>
2025-02-07 07:05:41 +0530

    changes

--- ps_hands-on-with-amazon-eks/Infrastructure/k8s-tooling/external-dns/create.sh
+++ ps_hands-on-with-amazon-eks/Infrastructure/k8s-tooling/external-dns/create.sh
@@ -1,2 +1,15 @@
 helm repo add external-dns https://kubernetes-sigs.github.io/external-dns/
 helm upgrade --install external-dns external-dns/external-dns --set policy="sync"
+
+# aws_r53_policy=$(aws cloudformation describe-stacks --stack-name aws-load-balancer-iam-policy --query "Stacks[*].Outputs[?OutputKey=='IamPolicyArn'].OutputValue" --output text | xargs)
+
+aws_r53_policy=$(aws iam create-policy --policy-name allow-externaldns-r53-iam-policy --policy-document file://AllowExternalDNS-R53.json --query "Stacks[*].Outputs[?OutputKey=='IamPolicyArn'].OutputValue" --output text | xargs)
+
+# Replaced name, namespace, cluster, IAM Policy arn 
+eksctl create iamserviceaccount \
+    --name external-dns \
+    --namespace default \
+    --cluster eks-acg \
+    --attach-policy-arn ${aws_r53_policy} \
+    --approve \
+    --override-existing-serviceaccounts
--- ps_hands-on-with-amazon-eks/Infrastructure/k8s-tooling/load-balancer-controller/map-iam-policy-to-ng-instance-role.sh
+++ ps_hands-on-with-amazon-eks/Infrastructure/k8s-tooling/load-balancer-controller/map-iam-policy-to-ng-instance-role.sh
@@ -1,5 +1,6 @@
-aws iam attach-role-policy \
-    --policy-arn $1 \
-    --role-name $2
+# Getting NodeGroup IAM Role from Kubernetes Cluster
+nodegroup_iam_role=$(aws cloudformation list-exports --query "Exports[?contains(Name, 'nodegroup-eks-node-group::InstanceRoleARN')].Value" --output text | xargs | cut -d "/" -f 2)
+aws_lb_controller_policy=$(aws cloudformation describe-stacks --stack-name aws-load-balancer-iam-policy --query "Stacks[*].Outputs[?OutputKey=='IamPolicyArn'].OutputValue" --output text | xargs)
+aws iam attach-role-policy --role-name ${nodegroup_iam_role} --policy-arn ${aws_lb_controller_policy}
 
 echo "IAM Policy mapped to NodeGroup Instance Role to get access to ALB Loadbalancer..."

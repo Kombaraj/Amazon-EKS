78c3624741d9a2e38a72b23899ed81189c5d802c
Your Name <you@example.com>
2025-02-08 20:29:54 +0530

    changes

--- ps_hands-on-with-amazon-eks/Infrastructure/k8s-tooling/map-iam-policy-to-ng-instance-role.sh
+++ ps_hands-on-with-amazon-eks/Infrastructure/k8s-tooling/map-iam-policy-to-ng-instance-role.sh
@@ -1,8 +1,8 @@
 # Getting NodeGroup IAM Role from Kubernetes Cluster
 nodegroup_iam_role=$(aws cloudformation list-exports --query "Exports[?contains(Name, 'nodegroup-eks-node-group::InstanceRoleARN')].Value" --output text | xargs | cut -d "/" -f 2)
-
+nodegroup_iam_role=$(aws cloudformation list-exports --query "Exports[?contains(Name, 'nodegroup-eks-node-group::InstanceRoleARN')].Value" --output text | xargs | cut -d "/" -f 2)
 aws iam attach-role-policy \
     --policy-arn arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess \
-    --role-name ${nodegroup_iam_role}}
+    --role-name ${nodegroup_iam_role}
 
 echo "IAM Policy mapped to NodeGroup Instance Role to get access to DynamoDB.."
